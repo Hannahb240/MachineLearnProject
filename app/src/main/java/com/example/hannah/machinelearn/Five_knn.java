bc020b5b58e9efbdc6483c388b7c7eada8a4e451
Hannah Berrisford <hannah.berrisford@student.manchester.ac.uk>
2018-02-21 11:59:29 +0000

    knn working

--- app/src/main/java/com/example/hannah/machinelearn/Five_knn.java
+++ app/src/main/java/com/example/hannah/machinelearn/Five_knn.java
@@ -1,12 +1,8 @@
 package com.example.hannah.machinelearn;
 
-import android.content.res.AssetManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
@@ -15,19 +11,15 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.google.android.gms.appindexing.Action;
-import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import org.opencv.*;
 import org.opencv.android.Utils;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.ml.KNearest;
 import org.opencv.ml.Ml;
+import org.opencv.utils.Converters;
 
-import java.io.IOException;
-import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Five_knn extends AppCompatActivity {
@@ -36,42 +28,26 @@ public class Five_knn extends AppCompatActivity {
     Mat sourceImage;
     TextView resultOfMat;
     ImageView myImageView;
+    ImageView myImageView2;
+    Mat trainData;
+    List<Integer> trainLabels;
+    List<Integer> testLabels;
 
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    private GoogleApiClient client;
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_five_knn);
+
         resultOfMat = (TextView) findViewById(R.id.mattext);
-        KNearest knn = KNearest.create();
 
-        Bitmap bitmap = null;
+        //Apple
+        Bitmap bitmap;
+        Drawable apple = getDrawable(R.drawable.person1);
 
-        Drawable apple = getDrawable(R.drawable.apple);
-
-
-        if (apple instanceof BitmapDrawable) {
-            BitmapDrawable bitmapDrawable = (BitmapDrawable) apple;
-            if (bitmapDrawable.getBitmap() != null) {
-                bitmap = bitmapDrawable.getBitmap();
-            }
-        }
-
-        if (apple.getIntrinsicWidth() <= 0 || apple.getIntrinsicHeight() <= 0) {
-            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
-        } else {
-            bitmap = Bitmap.createBitmap(apple.getIntrinsicWidth(), apple.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
-        }
-        Canvas canvas = new Canvas(bitmap);
-        apple.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-        apple.draw(canvas);
+        BitmapDrawable bitmapDrawable = (BitmapDrawable) apple;
+        bitmap = bitmapDrawable.getBitmap();
 
         myImageView = (ImageView) findViewById(R.id.myImgView);
         myImageView.setImageBitmap(bitmap);
@@ -79,26 +55,59 @@ public class Five_knn extends AppCompatActivity {
         Mat sourceImage = new Mat();
         Utils.bitmapToMat(bitmap, sourceImage);
 
-        resultOfMat.setText(sourceImage.dump());
+        sourceImage.convertTo(sourceImage, CvType.CV_32F);
+        trainData = new Mat();
+        trainData.push_back(sourceImage.reshape(1,1));
+
+        trainLabels= new ArrayList<Integer>();
+        trainLabels.add(1);
+
+
+        //pear
+        Bitmap bitmap2;
+        Drawable pear = getDrawable(R.drawable.person2);
+
+        BitmapDrawable bitmapDrawable2 = (BitmapDrawable) pear;
+        bitmap2 = bitmapDrawable2.getBitmap();
+
+        myImageView2 = (ImageView) findViewById(R.id.myImgView2);
+        myImageView2.setImageBitmap(bitmap2);
+
+        Mat sourceImage2 = new Mat();
+        Utils.bitmapToMat(bitmap2, sourceImage2);
+
+        sourceImage2.convertTo(sourceImage2, CvType.CV_32F);
+
+
+        trainData.push_back(sourceImage2.reshape(1,1));
+        trainLabels.add(2);
+
+        KNearest knn = KNearest.create();
+        knn.train(trainData, Ml.ROW_SAMPLE, Converters.vector_int_to_Mat(trainLabels));
+
+
+        Mat testData = new Mat();
+        testData.push_back(sourceImage2.reshape(1,1));
+
+        testLabels= new ArrayList<Integer>();
+        testLabels.add(2);
+
+
+        for (int i=0; i<testData.rows(); i++)
+        {
+            Mat one_feature = testData.row(i);
+            int testLabel = testLabels.get(i);
+
+            Mat res = new Mat();
+            float p = knn.findNearest(one_feature, 1, res);
+            resultOfMat.setText(testLabel + " " + p + " " + res.dump());
+
+        }
 
 
 
 
-        // knn.train(samples, Ml.ROW_SAMPLE, labels);
 
-
-        //Obtain training image 3 - standing
-//    AAsset *trainThree = AAssetManager_open(mgr, "stand3.JPG", AASSET_MODE_UNKNOWN);
-//    long sizeOfTrainThree = AAsset_getLength(trainThree);
-//    char *bufferTrainThree = (char *) AAsset_getBuffer(trainThree);
-//    std::vector<char> trainThreeData(bufferTrainThree, bufferTrainThree + sizeOfTrainThree);
-//    cv::Mat trainThreeMat = cv::imdecode(trainThreeData, IMREAD_UNCHANGED);
-//    AAsset_close (trainThree);
-
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public void clickhere(View view) {
@@ -106,39 +115,4 @@ public class Five_knn extends AppCompatActivity {
     }
 
 
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Five_knn Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
-    }
 }
