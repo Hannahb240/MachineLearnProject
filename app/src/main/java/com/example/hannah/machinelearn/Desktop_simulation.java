8663b5f9449d132f8bdedd7320d7bbafe4765965
Hannah Berrisford <hannah.berrisford@student.manchester.ac.uk>
2018-02-22 23:28:52 +0000

    Desktop simulation imageButtons working

---
+++ app/src/main/java/com/example/hannah/machinelearn/Desktop_simulation.java
@@ -0,0 +1,13 @@
+package com.example.hannah.machinelearn;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Desktop_simulation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desktop_simulation);
+    }
+}
